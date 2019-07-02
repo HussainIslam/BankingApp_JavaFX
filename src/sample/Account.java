5c1aa62fcc9fe283ae7fcc745c41b91265060638
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2019-07-02 11:51:11 -0400

    Account class working including serialization and desrialization

--- src/sample/Account.java
+++ src/sample/Account.java
@@ -1,5 +1,68 @@
 package sample;
 
-public class Account {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Account implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double balance;
+    private static double annualInterestRate;
+    private Date dateCreated;
+
+    public Account(){
+        id = 0;
+        balance = 0.00;
+    }
+
+    public Account(int id, double balance){
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public static double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public static void setAnnualInterestRate(double annualInterestRate) {
+        Account.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterestRate(){
+        return annualInterestRate /12;
+    }
+
+    public double getMonthlyInterest(){
+        return this.balance * getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount){
+        this.balance -= amount;
+    }
+
+    public void deposit (double amount){
+        this.balance += amount;
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,14 +6,42 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
+
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        Account[] accountList = new Account[10];
+        for(int i = 0; i < 10; i++){
+            accountList[i] = new Account(i, 100);
+        }
+        /*for(int i = 0; i < 10; i++){
+            System.out.println("Account " +i +" has balance of " +accountList[i].getBalance());
+        }*/
+        String fileName = "account.dat";
+
+        //Serialization
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))){
+            for(int i = 0; i < 10; i++){
+                out.writeObject(accountList[i]);
+            }
+            System.out.println("Objects has been serialized");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Account temp = null;
+        //Deserialization
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName))){
+            for(int i = 0; i < 10; i++){
+                temp = (Account) in.readObject();
+                System.out.println("Account number " +temp.getId() +" has balance of " +temp.getBalance());
+            }
+        }
+
     }
 
 
